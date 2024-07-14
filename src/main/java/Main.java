ef68c2793ebbd787f90b460341df3ba85a8f8798
ynoolee <heerarea129@gmail.com>
2024-07-14 12:32:24 +0900

    refactor: [Respond with 200] replace with BufferedOutputStream

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,30 @@
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
-        //
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        //
-        try {
-            serverSocket = new ServerSocket(4221);
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
-
-            /* bind(bind to address) -> liste() -> accept() -> send() , recv() */
+        /* bind(bind to address) -> listen() -> accept() -> send() , recv() */
+        try (var serverSocket = new ServerSocket(4221)) {
             serverSocket.setReuseAddress(true);
 
-            clientSocket = connect(serverSocket);
+            Socket clientSocket = connectToServerSocket(serverSocket);
 
             final String httpMessage = "HTTP/1.1 200 OK\r\n\r\n";
-            final PrintStream output = new PrintStream(clientSocket.getOutputStream());
-            output.println(httpMessage);
+
+
+            // my version 1 : using PrintStream
+//            final PrintStream output = new PrintStream(clientSocket.getOutputStream());
+//            output.println(httpMessage);
+//            output.flush();
+
+            // my version 2 : using BufferedOutputStream
+            final BufferedOutputStream bufferedOut = new BufferedOutputStream(clientSocket.getOutputStream());
+            bufferedOut.write(httpMessage.getBytes());
+            bufferedOut.flush();
 
             clientSocket.close();
         } catch (IOException e) {
@@ -32,7 +32,7 @@ public class Main {
         }
     }
 
-    private static Socket connect(final ServerSocket serverSocket) throws IOException {
+    private static Socket connectToServerSocket(final ServerSocket serverSocket) throws IOException {
         Socket clientSocket;
         clientSocket = serverSocket.accept(); // Wait for connection from client.
         System.out.println("accepted new connection");
