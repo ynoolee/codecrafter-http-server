e9dfb9aefb12723a5ef160c40f9c358b96bb7a88
ynoolee <heerarea129@gmail.com>
2024-07-28 13:09:20 +0900

    refactor : extract method

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,36 +10,55 @@ public class HttpServer {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("HTTP server started on port " + port);
 
+
             try (final Socket clientSocket = serverSocket.accept();
                  final InputStream inputStream = clientSocket.getInputStream()
             ) {
-                final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                final char[] tempMessageContainer = new char[4096];
-                final char[] finalMessage;
-                int curIdx = 0;
-
-                while (true) {
-                    char readChar = (char) reader.read();
-
-                    tempMessageContainer[curIdx++] = readChar;
-
-                    if (readChar == '\n') {
-                        if (curIdx >= 4
-                                && tempMessageContainer[curIdx - 2] == '\r'
-                                && tempMessageContainer[curIdx - 3] == '\n'
-                                && tempMessageContainer[curIdx - 4] == '\r') {
-                            break;
-                        }
-                    }
-                }
-                finalMessage = new char[curIdx - 1];
-
-                System.arraycopy(tempMessageContainer, 0, finalMessage, 0, finalMessage.length);
-                System.out.println(finalMessage);
+                final String message = readHttpMessageNotContainingBody(inputStream);
+                System.out.println(message);
             }
             System.out.println("Turn off HTTP Server");
         } catch (IOException e) {
             System.err.println("Server error: " + e.getMessage());
         }
+    }
+
+    /**
+     * In this case where Http Message does not contain any message body,
+     * Just detecting Header terminator ( CRLF Followed by CRLF )
+     * is only thing reading Http message.
+     * */
+    private static String readHttpMessageNotContainingBody(InputStream inputStream) throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        final char[] tempMessageContainer = new char[4096];
+        int curIdx = 0;
+
+        while (true) {
+            char readChar = (char) reader.read();
+
+            tempMessageContainer[curIdx++] = readChar;
+
+            // detect Header terminator
+            if (readChar == '\n') {
+                if (curIdx >= 4
+                        && tempMessageContainer[curIdx - 2] == '\r'
+                        && tempMessageContainer[curIdx - 3] == '\n'
+                        && tempMessageContainer[curIdx - 4] == '\r') {
+                    break;
+                }
+            }
+        }
+
+        char[] finalMessage = truncateAndCreateArray(tempMessageContainer, curIdx -1);
+
+        return new String(finalMessage);
+    }
+
+    private static char[] truncateAndCreateArray(char[] array, int length) {
+        final char[] result = new char[length];
+
+        System.arraycopy(array, 0, result, 0, result.length);
+
+        return result;
     }
 }
