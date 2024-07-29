3290c0e72c49bb5ce6b0f95aa9a7e6ed360e13e0
ynoolee <heerarea129@gmail.com>
2024-07-29 20:17:24 +0900

    feat: [Return a file #AP6]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int count = 0;
         for (String arg : args) {
-            System.out.println("arg : "+ arg);
+            System.out.printf("%dth arg : %s\n", count++, arg);
         }
         final HttpServer httpServer = new HttpServer();
         httpServer.run(4221);
