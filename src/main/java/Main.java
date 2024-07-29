985deb16f677f7dc31891078add9f16fa2787c15
ynoolee <heerarea129@gmail.com>
2024-07-29 20:15:48 +0900

    feat: [Return a file #AP6]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("arg : "+ arg);
+        }
         final HttpServer httpServer = new HttpServer();
         httpServer.run(4221);
     }
