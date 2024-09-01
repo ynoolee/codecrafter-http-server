a0e29276918475717c1c9871bcff0ab4273f11b3
ynoolee <heerarea129@gmail.com>
2024-09-01 17:30:27 +0900

    feat: nio Buffer 를 통해 데이터를 읽는 HttpMessage 파싱 기능 추가

--- src/main/java/HttpMessageParseUtil.java
+++ src/main/java/HttpMessageParseUtil.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.CharBuffer;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -21,13 +22,36 @@ public class HttpMessageParseUtil {
         }
 
         final int contentLength = request.valueOfKey(HttpHeader.CONTENT_LENGTH)
-                .map(Integer::valueOf).orElse(0);
+            .map(Integer::valueOf).orElse(0);
 
         logger.info("content length : " + contentLength);
         StringBuilder content = new StringBuilder();
         int contentSize = 0;
         while (contentSize < contentLength) {
             String readCharStr = String.valueOf((char) reader.read());
+            int readCharSize = readCharStr.getBytes().length;
+            content.append(readCharStr);
+            contentSize += readCharSize;
+        }
+
+        return request.withBody(content.toString());
+    }
+
+    public static HttpRequest readHttpRequestMessage(CharBuffer buffer) throws IOException {
+        HttpRequest request = createHttpRequestUntilBody(buffer);
+
+        if (request.hasEmptyBody()) {
+            return request;
+        }
+
+        final int contentLength = request.valueOfKey(HttpHeader.CONTENT_LENGTH)
+            .map(Integer::valueOf).orElse(0);
+
+        logger.info("content length : " + contentLength);
+        StringBuilder content = new StringBuilder();
+        int contentSize = 0;
+        while (contentSize < contentLength) {
+            String readCharStr = String.valueOf(buffer.get());
             int readCharSize = readCharStr.getBytes().length;
             content.append(readCharStr);
             contentSize += readCharSize;
@@ -46,18 +70,38 @@ public class HttpMessageParseUtil {
             sb.append(readChar);
             curIdx++;
             // detect Header terminator
-            if (readChar == '\n') {
-                if (curIdx >= 4
-                        && sb.charAt(curIdx - 2) == '\r'
-                        && sb.charAt(curIdx - 3) == '\n'
-                        && sb.charAt(curIdx - 4) == '\r'
-                ) {
-                    break;
-                }
+            if (isHeaderTermination(readChar, sb, curIdx)) {
+                break;
             }
         }
 
         return createHttpRequest(sb.toString());
+    }
+
+    private static HttpRequest createHttpRequestUntilBody(CharBuffer buffer) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int curIdx = 0;
+
+        while (true) {
+            char readChar = (char) buffer.get();
+            sb.append(readChar);
+            curIdx++;
+            if (isHeaderTermination(readChar, sb, curIdx)) {
+                break;
+            }
+        }
+
+        return createHttpRequest(sb.toString());
+    }
+
+    private static boolean isHeaderTermination(char readChar, StringBuilder sb, int curIdx) {
+        if (readChar == '\n') {
+            return curIdx >= 4
+                && sb.charAt(curIdx - 2) == '\r'
+                && sb.charAt(curIdx - 3) == '\n'
+                && sb.charAt(curIdx - 4) == '\r';
+        }
+        return false;
     }
 
     private static HttpRequest createHttpRequest(final String startLineAndHeaders) {
@@ -67,26 +111,18 @@ public class HttpMessageParseUtil {
         final Headers headers = createHeaders(lines.subList(1, lines.size())); // The rest part is headers
 
         return HttpRequest.builder()
-                .startLine(startLine)
-                .headers(headers.deepCopy())
-                .build();
+            .startLine(startLine)
+            .headers(headers.deepCopy())
+            .build();
     }
 
     private static Headers createHeaders(final List<String> headers) {
         Map<HttpHeader, String> map = new HashMap<>();
 
         headers.stream()
-                .map(headerLine -> Arrays.stream(headerLine.split(":")).map(String::trim).toArray(String[]::new))
-                .forEach(headerKeyValue -> HttpHeader.of(headerKeyValue[0]).map(key -> map.put(key, headerKeyValue[1])));
+            .map(headerLine -> Arrays.stream(headerLine.split(":")).map(String::trim).toArray(String[]::new))
+            .forEach(headerKeyValue -> HttpHeader.of(headerKeyValue[0]).map(key -> map.put(key, headerKeyValue[1])));
         return new Headers(map);
-    }
-
-    private static char[] clearTheRestPart(char[] array, int length) {
-        final char[] result = new char[length];
-
-        System.arraycopy(array, 0, result, 0, result.length);
-
-        return result;
     }
 
     private static StartLine createStartLine(List<String> messageParts) {
